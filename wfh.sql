fa6b955d7cfcfe69bb3a33aa7659eb809300eb6e
genald <genald21@gmail.com>
2020-11-03 12:03:01 +0800

    Updated Database

--- wfh.sql
+++ wfh.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 29, 2020 at 05:11 PM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.3.9
+-- Generation Time: Nov 03, 2020 at 05:02 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,19 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `wfh`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `manageraccounts`
---
-
-CREATE TABLE `manageraccounts` (
-  `managerID` varchar(30) DEFAULT NULL,
-  `name` varchar(30) DEFAULT NULL,
-  `password` varchar(100) DEFAULT NULL,
-  `role` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -54,7 +41,7 @@ CREATE TABLE `useraccounts` (
 --
 
 INSERT INTO `useraccounts` (`accountID`, `name`, `password`, `role`, `managerID`) VALUES
-('Aeighy', 'Adrian', '$2y$10$ScD0tW2vuQHf4xIF5k5mSuKXi0J8g31EHlAEnif8emX/pqZdk7hHq', 0, NULL);
+('manager', 'Manager', '$2y$10$doCYJJeqcBJxSJR3SbTit.kdXrjkQhUsZtMJebfq/0KWqX55YdB1m', 1, '');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
